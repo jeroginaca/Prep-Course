39e5e4add5b4eda4f3c8e6dc8b6d6b92df63d684
Jeronimo <108022277+jeroginaca@users.noreply.github.com>
2022-07-01 12:29:55 +0200

    Jeronimo Prep Course

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,18 +3,21 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+  return array[0];
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return array [array.length-1];
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return array.length;
 }
 
 
@@ -23,6 +26,12 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+
+  var nuevoArray = [];
+  for (let i = 0; i <array.length; i++ ){
+    nuevoArray[i] = array [i] + 1;
+  }
+  return nuevoArray;
 }
 
 
@@ -30,7 +39,10 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+ array.push(elemento);
+ return array;
 }
+
 
 
 function agregarItemAlComienzoDelArray(array, elemento) {
@@ -38,6 +50,10 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  
+  array.unshift(elemento);
+  return array;
+
 }
 
 
@@ -47,6 +63,11 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+  return palabras.join(" ");
+  
+
+
+
 }
 
 
@@ -54,6 +75,13 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+   
+for(var i=0; i < array.length; i++){
+  if(array[i] === elemento) return true;
+}
+return false;
+  
+ 
 }
 
 
@@ -61,6 +89,12 @@ function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  var arr = 0;
+  for(var i= 0; i<numeros.length; i++){
+    arr = arr + numeros[i];
+  }
+  return arr;
+  
 }
 
 
@@ -75,6 +109,7 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  
 }
 
 
@@ -82,13 +117,14 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  
 }
 
 
@@ -113,8 +149,7 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
-} 
+}
 
 
 function mesesDelAño(array) {
@@ -122,6 +157,7 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  
 }
 
 
@@ -129,6 +165,7 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  
 }
 
 
@@ -140,6 +177,7 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  
 }
 
 
@@ -150,6 +188,7 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  
 }
 
 
---
+++ 04-JS-III/pruebas.js
@@ -0,0 +1,44 @@
+var nombre = "Jero";
+let apellido = "Ginaca";
+const comidafavorita = "Hamburguesa";
+
+console.log();
+
+var nombrePerro = "RAMBO";
+
+var positivo = 2;
+var negativo = -1;
+
+var meEncantaJavascript = true;
+
+var a = 2 + 3;
+var b = 3 / 3;
+
+Math.pow(2,2) = 4;
+Math.pow(3,2) = 9;
+
+Math.round (6.5) = 7;
+Math.round (6.45) = 6;
+Math.floor (6.99) = 6;
+Math.ceil (6.001) = 7;
+
+var nombreGato = "feliz";
+console.log(nombreGato.length); // 5
+
+function miFuncion(){}
+var otraFuncion = function () {};
+var yOtra = () => {};
+
+function logHola() {
+    console.log("hola");
+}
+
+logHola();
+
+function logHola(nombre) {
+    console.log("hola, " + nombre);
+
+}
+
+logHola("Martin");
+
